c0e6bf449f8401789a320c046af34a5b1d746a80
mark-tw <45938825+mark-tw@users.noreply.github.com>
2019-04-06 05:02:17 +0300

    Default Changelist

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -2,12 +2,12 @@
 .modal
     .Manage-rules
         | Manage rules
-        .Rectangle
+    .Rectangle
     .tooltip
-    tooltip-input(
-    v-for="label in labels"
-    :label="label"
-    )
+        tooltip-input(
+        v-for="label in labels"
+        :label="label"
+        )
 </template>
 
 <script>
@@ -17,7 +17,7 @@ export default {
   components: {TooltipInput},
   data () {
     return {
-      labels: ['duration', 'min to use', 'max to use'],
+      labels: ['duration', 'min. votes to pass', 'votes per user'],
       modalOpened: false,
       isActive: false
     }
@@ -30,78 +30,68 @@ export default {
 }
 </script>
 
-<style scoped>
-.modal {
-    position: relative;
-    margin-top: 13px;
-    transition: opacity 1s ease, transform 1s ease;
-    border: solid 1px #c8d2dd;
-    width: 216px;
-    height: 234px;
-    border-radius: 5px;
-    box-shadow: 2px 3px 7px 2px rgba(0, 0, 0, 0.05), 0 2px 12px 0 rgba(132, 149, 163, 0.32);
-    background-color: #ffffff;
+<style lang='sass' scoped>
+    @import url('https://fonts.googleapis.com/css?family=Lato')
+    .modal
+        position: relative
+        margin-top: 13px
+        transition: opacity 1s ease, transform 1s ease
+        border: solid 1px #c8d2dd
+        width: 216px
+        height: 234px
+        border-radius: 5px
+        box-shadow: 2px 3px 7px 2px rgba(0, 0, 0, 0.05), 0 2px 12px 0 rgba(132, 149, 163, 0.32)
+        background-color: #ffffff
+        &:before
+            content: ''
+            display: block
+            position: absolute
+            left: 97px
+            bottom: 100%
+            width: 0
+            height: 0
+            border: 11px solid transparent
+            border-bottom-color: #c8d2dd
+        &:after
+            content: ''
+            display: block
+            position: absolute
+            left: 99px
+            bottom: 100%
+            width: 0
+            height: 0
+            border: 9px solid transparent
+            border-bottom-color: white
 
-}
+    .modal-transition-enter-active
+        opacity: 1
 
-.modal:before {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 97px;
-    bottom: 100%;
-    width: 0;
-    height: 0;
-    border: 11px solid transparent;
-    border-bottom-color: #c8d2dd;
-}
+    .modal-transition-enter, .modal-transition-leave-active
+        opacity: 0
 
-.modal:after {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 99px;
-    bottom: 100%;
-    width: 0;
-    height: 0;
-    border: 9px solid transparent;
-    border-bottom-color: white;
-}
+    .modal-transition-leave
+        opacity: 1
 
-.modal-transition-enter-active {
-    opacity: 1;
+    .Manage-rules
+        margin-top: 9px
+        margin-left: 16px
+        margin-bottom: 9px
+        width: 85px
+        height: 20px
+        font-family: Lato
+        font-size: 14px
+        font-weight: bold
+        font-style: normal
+        font-stretch: normal
+        line-height: 1.43
+        letter-spacing: normal
+        color: #3c4a5a
 
-}
-
-.modal-transition-enter{
-    opacity: 0;
-
-}
-.modal-transition-leave-active {
-    opacity: 0;
-
-}
-.modal-transition-leave{
-    opacity: 1;
-}
-.Manage-rules{
-    margin-top: 9px;
-    margin-left: 17px;
-    margin-bottom: 10px;
-    width: 85px;
-    height: 20px;
-    font-family: Lato;
-    font-size: 14px;
-    font-weight: bold;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.43;
-    letter-spacing: normal;
-    color: #3c4a5a;
-
-}
-.Rectangle{
-    border-bottom: 1px solid #e8ecf2;
-    width: 216px;
-}
+    .Rectangle
+        border-bottom: 1px solid #e8ecf2
+        width: 216px
+        height: 1px
+    .tooltip
+        margin-top: 17px
+        margin-left: 16px
 </style>
--- src/components/TooltipInput.vue
+++ src/components/TooltipInput.vue
@@ -1,17 +1,18 @@
-<template lang="pug">
+<template lang='pug'>
     label(
     :class="{active: hasFocus}"
     ) {{label}}
         .input-wrapper
-            input.input-tooltip(
+          input.input-tooltip(
             :value="inputValue"
             @input="onInput"
             @focus="hasFocus = true"
             @blur="hasFocus = false"
             ref="input"
-            )
-            .input-mask
-                span {{inputValue}}
+            maxlength="20"
+        )
+        .input-mask
+          span {{inputValue}}
 </template>
 
 <script>
@@ -44,65 +45,61 @@ export default {
 }
 </script>
 
-<style scoped>
-.input-mask {
-    border-bottom: 1px solid blue;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    pointer-events: none;
-    opacity: 0;
-}
-.input-wrapper {
-    width: fit-content;
-    height: fit-content;
-    position: relative;
-}
-.input-mask span {
-    opacity: 0;
-    font-size: inherit;
-    font-family: inherit;
-}
-input{
-    font-size: inherit;
-    font-family: inherit;
-}
-.active{
-    color: blue;
+<style lang='sass' scoped>
+    @import url('https://fonts.googleapis.com/css?family=Lato')
+    .input-mask
+        border-bottom: 2px solid #2db9f0
+        position: absolute
+        top: 0
+        left: 0
+        height: 100%
+        pointer-events: none
+        opacity: 1
+    .input-wrapper
+        width: fit-content
+        height: fit-content
+        position: relative
+    .input-mask span
+        opacity: 1
+        font-size: inherit
+        font-family: inherit
+    .active
+        color: #2db9f0
+        .input-mask
+            opacity: 1
 
-}
-.active .input-mask{
-    opacity: 1;
-}
-label:focus-within{
-    color: #2db9f0;
-}
-input{
-    border-top: none;
-    border-left: none;
-    border-right: none;
-    border-color: white;
-    outline:none;
-    font-family: Lato;
-    font-size: 14px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.43;
-    letter-spacing: normal;
-    margin-top: 6px;
-    color: #3c4a5a;
+    label:focus-within
+        color: #2db9f0
 
-}
-input:focus{
-    caret-color: #2db9f0;
-    font-family: Lato;
-    font-size: 14px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.43;
-    letter-spacing: normal;
-}
+    input
+        border-top: none
+        border-left: none
+        border-right: none
+        border-color: white
+        outline: none
+        font-family: inherit
+        font-size: inherit
+        color: #3c4a5a
+        &:focus
+            caret-color: #2db9f0
+
+    label
+        text-transform: uppercase
+        font-family: Lato
+        font-size: 11px
+        font-weight: bold
+        font-style: normal
+        font-stretch: normal
+        line-height: normal
+        letter-spacing: 0.9px
+        color: #3c4a5a
+
+    .input-tooltip
+        font-family: Lato
+        font-size: 14px
+        font-weight: normal
+        font-style: normal
+        font-stretch: normal
+        line-height: 1.43
+        letter-spacing: normal
 </style>
